ffabbaadef901239487734e9a6a793bb5058175b
Sergio Márquez <smarquezs@gmail.com>
2022-01-13 20:42:03 -0300

    Save content to a json file.

--- hello/lib/hello/import_movies.ex
+++ hello/lib/hello/import_movies.ex
@@ -2,13 +2,35 @@ defmodule Hello.ImportMovies do
   def import() do
     api_key = System.get_env("MOVIE_API_KEY")
 
-    api_url =
-      "https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}&language=es-ES&page=1"
+    movies =
+      Enum.reduce(1..150, [], fn page, list ->
+        api_url =
+          "https://api.themoviedb.org/3/movie/popular?api_key=#{api_key}&language=es-ES&page=#{
+            page
+          }"
 
-    response = HTTPoison.get!(api_url)
+        IO.puts("Fetching page #{page}")
+        response = HTTPoison.get!(api_url)
+        response = response.body |> Jason.decode!()
 
-    response.body
-    |> Jason.decode!()
-    |> IO.inspect()
+        results =
+          response["results"]
+          |> Enum.map(fn movie ->
+            %{
+              title: movie["title"],
+              overview: movie["overview"],
+              id: movie["id"],
+              vote_average: movie["vote_average"],
+              vote_count: movie["vote_count"],
+              page: response["page"],
+              release_date: movie["release_date"]
+            }
+          end)
+
+        list ++ results
+      end)
+
+    movies = Jason.encode!(movies)
+    File.write!("/tmp/movies.json", movies, [:write])
   end
 end
