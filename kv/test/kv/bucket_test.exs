d3c508334bc13c4f1d2f7409f65a699cda551622
Sergio Márquez <smarquezs@gmail.com>
2022-02-13 12:54:27 -0300

    Learn how to use Agents.

---
+++ kv/test/kv/bucket_test.exs
@@ -0,0 +1,18 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+
+    assert KV.Bucket.delete(bucket, "milk") == 3
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+end
